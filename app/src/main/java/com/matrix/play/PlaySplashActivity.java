c671e618c9c609ab90b907239233ae05e8b96c8b
Sunil Tiwari <fofadiyapayal@gmail.com>
2020-08-11 16:13:49 -0700

    some cosmatic changes (#4)

--- app/src/main/java/com/matrix/play/PlaySplashActivity.java
+++ app/src/main/java/com/matrix/play/PlaySplashActivity.java
@@ -9,6 +9,7 @@ import com.viksaa.sssplash.lib.model.ConfigSplash;
 
 import static com.matrix.play.Constants.ANIMATION_CIRCULAR_REVEAL_DURATION;
 import static com.matrix.play.Constants.ANIMATION_LOGO_SPLASH_DURATION;
+import static com.matrix.play.Constants.ANIMATION_TITLE_DURATION;
 
 
 public class PlaySplashActivity extends AwesomeSplash {
@@ -17,21 +18,21 @@ public class PlaySplashActivity extends AwesomeSplash {
     public void initSplash(ConfigSplash configSplash) {
 
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.White);
+        configSplash.setBackgroundColor(R.color.colorPrimary);
         configSplash.setAnimCircularRevealDuration(ANIMATION_CIRCULAR_REVEAL_DURATION);
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM);
 
         //Customize Logo
-        configSplash.setLogoSplash(R.mipmap.ic_app_launcher);
+        configSplash.setLogoSplash(R.drawable.ic_app_splash);
         configSplash.setAnimLogoSplashDuration(ANIMATION_LOGO_SPLASH_DURATION);
         configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn);
 
         //Customize Title
         configSplash.setTitleSplash(getString(R.string.text_splash_screen_title));
-        configSplash.setTitleTextColor(R.color.colorPrimary);
-        configSplash.setTitleTextSize(15f);
-        configSplash.setAnimTitleDuration(ANIMATION_LOGO_SPLASH_DURATION);
+        configSplash.setTitleTextColor(R.color.White);
+        configSplash.setTitleTextSize(30f);
+        configSplash.setAnimTitleDuration(ANIMATION_TITLE_DURATION);
         configSplash.setAnimTitleTechnique(Techniques.FadeIn);
 
     }
