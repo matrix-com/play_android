a5f934ae86aa9497bd1622081315048bbf0d512e
Sunil Tiwari <fofadiyapayal@gmail.com>
2020-08-11 12:03:46 -0700

    App-3-Customize-Splash-Screen (#3)
    * some minor fix
    
    * space issue fixed

---
+++ app/src/main/java/com/matrix/play/Constants.java
@@ -0,0 +1,9 @@
+package com.matrix.play;
+
+public class Constants {
+
+    //Splash Screen Constants
+    public static final int ANIMATION_CIRCULAR_REVEAL_DURATION = 1000;
+    public static final int ANIMATION_LOGO_SPLASH_DURATION = 500;
+    public static final int ANIMATION_TITLE_DURATION = 500;
+}
--- app/src/main/java/com/matrix/play/PlaySplashActivity.java
+++ app/src/main/java/com/matrix/play/PlaySplashActivity.java
@@ -7,6 +7,9 @@ import com.viksaa.sssplash.lib.activity.AwesomeSplash;
 import com.viksaa.sssplash.lib.cnst.Flags;
 import com.viksaa.sssplash.lib.model.ConfigSplash;
 
+import static com.matrix.play.Constants.ANIMATION_CIRCULAR_REVEAL_DURATION;
+import static com.matrix.play.Constants.ANIMATION_LOGO_SPLASH_DURATION;
+
 
 public class PlaySplashActivity extends AwesomeSplash {
 
@@ -14,26 +17,22 @@ public class PlaySplashActivity extends AwesomeSplash {
     public void initSplash(ConfigSplash configSplash) {
 
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.white); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(1000); //int ms
-        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
-        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
-
-        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+        configSplash.setBackgroundColor(R.color.White);
+        configSplash.setAnimCircularRevealDuration(ANIMATION_CIRCULAR_REVEAL_DURATION);
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM);
 
         //Customize Logo
-        configSplash.setLogoSplash(R.mipmap.ic_app_launcher); //or any other drawable
-        configSplash.setAnimLogoSplashDuration(1000); //int ms
-        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
-
+        configSplash.setLogoSplash(R.mipmap.ic_app_launcher);
+        configSplash.setAnimLogoSplashDuration(ANIMATION_LOGO_SPLASH_DURATION);
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn);
 
         //Customize Title
-        configSplash.setTitleSplash("Fun Begins Here...!");
+        configSplash.setTitleSplash(getString(R.string.text_splash_screen_title));
         configSplash.setTitleTextColor(R.color.colorPrimary);
-        configSplash.setTitleTextSize(20f); //float value
-        //configSplash.setAnimTitleDuration(1000);
-        //configSplash.setAnimTitleTechnique(Techniques.FadeOut);
-        //configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
+        configSplash.setTitleTextSize(15f);
+        configSplash.setAnimTitleDuration(ANIMATION_LOGO_SPLASH_DURATION);
+        configSplash.setAnimTitleTechnique(Techniques.FadeIn);
 
     }
 
